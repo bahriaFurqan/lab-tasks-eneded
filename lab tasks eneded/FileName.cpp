6de081cbf43d2e7703411bb22ebdf0f811938ed9
Furqan Ahmad <ahmedfurqan679@gmail.com>
2023-11-02 17:34:55 +0500

    change

--- lab tasks eneded/FileName.cpp
+++ lab tasks eneded/FileName.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	int option = 0,issue_date,return_date ;
+	int days_book_borrowed,payment=0 ;
 	string book_name,book_return ;
 	cout << "Press 1 if you want to borrow book?\n";
 	cout << "Press 2 if you want to return book?\n";
@@ -25,8 +26,24 @@ int main()
 		cout << "Enter the Issue date of book: \n";
 		cin >> issue_date;
 		cout << "Enter the return date of book: \n";
-		cin >> return_date ;
+		cin >> return_date;
+		days_book_borrowed = return_date - issue_date;
+
+		if (days_book_borrowed > 14)
+		{
+			cout << "Book not successfully returned\n";
+			payment = days_book_borrowed * 1;
+			cout << "you have to pay= $" << payment <<"\n";
+
+		}
+		else if(days_book_borrowed < 14)
+		{
+			cout << "You have successfully submitted the book ";
+		}
+		system("pause");
+			return 0;
 
 	}
+
 
 }
